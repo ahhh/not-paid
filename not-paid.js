703d959e8e7e3d1ad6d7aa38431bd238d56aca06
Dan Borges <ahhh.db@gmail.com>
2019-02-06 09:36:25 -0800

    Update not-paid.js

--- not-paid.js
+++ not-paid.js
@@ -1,16 +1,16 @@
 (function(){
 	/* change these variables as you wish */
+	var hours_date = 1549479538;
 	var hours_deadline = 2;
 	/* stop changing here */
 	
 	var current_date = new Date();
 	var utc1 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
-	var utc2 = utc1 + (hours_deadline * 60 * 60)
-	var hours = Math.floor((utc2 - utc1) / (60 * 60));
+	var utc2 = (hours_date)
+	var hours = Math.floor((utc2 - (utc1/1000)) / (60 * 60));
 	
 	if(hours > 0) {
-		var hours_late = hours_deadline-hours;
-		var opacity = (hours_late*100/hours_deadline)/100;
+		var opacity = (hours*100/hours_deadline)/100;
 			opacity = (opacity < 0) ? 0 : opacity;
 			opacity = (opacity > 1) ? 1 : opacity;
 		if(opacity >= 0 && opacity <= 1) {
